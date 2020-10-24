4c03fb7d8dc3759a0ddf45bb0ac3ae7778ef02cb
Yaman Boztepe <yaman.boztepe@gmail.com>
2020-10-24 14:20:47 +0300

    Initial Commit

---
+++ LiftHeavier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiftHeavier
+//
+//  Created by Yaman Boztepe on 24.10.2020.
+//  Copyright © 2020 Yaman Boztepe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
